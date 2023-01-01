3b7a7094319118e262b27a7b8e81e5b9e4cbfb1b
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-01-02 00:29:15 +0530

    op

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from bypasser import ddllist
 
 
 # bot
-bot_token = os.environ.get("TOKEN", "5724481282:AAED4prRP21THu1pZ9EpIqzum6L7xQHsphw")
+bot_token = os.environ.get("TOKEN", "5860879943:AAHvB9XGBpBypBOHDZY6x8XJTrwDhfsmXlU")
 api_hash = os.environ.get("HASH", "1c521005e55c95e6973ae714d2e9e424") 
 api_id = os.environ.get("ID", "7234399")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
@@ -53,7 +53,7 @@ def loopthread(message):
 # start command
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    app.send_message(message.chat.id, f"__👋 Hi Op **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
+    app.send_message(message.chat.id, f"__👋 Hey **{message.from_user.mention}**,  You join update channel @Sehrish_Noor & use bot.\nCheckout /help to Read More__",
     reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🧑‍💻 Owner", url="https://t.me/Opleech")]]), reply_to_message_id=message.id)
 
 
